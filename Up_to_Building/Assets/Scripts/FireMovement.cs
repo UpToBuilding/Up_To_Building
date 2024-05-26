32226d7e23619dd63e30caeceb075e313c43e248
악당커비 <69393156+villainouskirby@users.noreply.github.com>
2024-05-26 15:56:04 +0900

    설정 UI 추가

--- Up_to_Building/Assets/Scripts/FireMovement.cs
+++ Up_to_Building/Assets/Scripts/FireMovement.cs
@@ -15,7 +15,7 @@ public class FireMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigid.velocity = direction * 3;
+        rigid.velocity = direction * 5;
     }
 
     // Update is called once per frame
@@ -26,17 +26,14 @@ public class FireMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        float colPos = col.transform.position.y;
+        /*float colPos = col.transform.position.y;
         float colHalfSize = col.transform.localScale.y / 2;
         float firePos = transform.position.y;
         if (!col.gameObject.CompareTag("Player") && colPos - colHalfSize < firePos && colPos + colHalfSize > firePos) // 맵 형태에 따라 세밀한 조정 필요
         {
             destroyFire();
-        }
-        if (col.gameObject.tag == "Ground")
-        {
-            rigid.AddForce(Vector2.up * 3, ForceMode2D.Impulse);
-        }
+        }*/
+        destroyFire();
     }
 
     public void setDirection(Vector3 direction)
